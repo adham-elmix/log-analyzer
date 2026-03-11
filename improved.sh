f405d89721fbeb4e0537b76238aaaabdc9c52b9f
Adham Elmix <shotlive12@gmail.com>
2026-03-11 11:00:58 +0200

    Update improved.sh

--- improved.sh
+++ improved.sh
@@ -1,11 +1,9 @@
-
 #!/bin/bash
 log_file="log.txt"
 echo "Top IPs:"
-grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" log.txt | sort | uniq -c | sort -nr
+grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" "$log_file" | sort | uniq -c | sort -nr
 echo "Failed logins:"
-grep "failed" log.txt | wc -l
+grep "failed" "$log_file" | wc -l
 echo "Unique IPs:"
-grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" log.txt | sort -u | wc -l
-echo "IPs suspected of Brute Force attempts (more than 3 failed logins): $(grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" $log_file | sort | uniq -c | sort -nr | awk '$1>3 {print "ip :",$2," Number of attempts : " , $1}'
-)"
+grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" "$log_file"  | sort -u | wc -l
+echo "IPs suspected of Brute Force attempts (more than 3 failed logins):\n$(grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}" "$log_file" | sort | uniq -c | sort -nr | awk '$1>3 {print "IP:", $2, "Attempts:", $1}')"
